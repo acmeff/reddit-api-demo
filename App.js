9d466306b803754b6affb189ce71490ea63097c5
Audrey Mefford <acmefford@gmail.com>
2017-07-15 16:57:11 -0700

    Initial commit. Set up app with Create React Native. Test fetching from reddit-api

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+
+  componentDidMount() {
+    let feed = fetch('https://www.reddit.com/.json').then(res => res['data']);
+    console.log(`feed: ${feed}`);
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make automatically reload.</Text>
+        <Text>Shake your phone open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
